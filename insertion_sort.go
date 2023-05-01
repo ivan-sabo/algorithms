0c95bd952eec4613e18f47d93310d447cc5f30c7
Ivan Sabo <123sabo@gmail.com>
2023-05-01 13:30:06 +0200

    insertion sort implementation and unit tests

--- insertion_sort.go
+++ insertion_sort.go
@@ -1,1 +1,21 @@
 package main
+
+type InsertionSort struct {
+	s []int
+}
+
+func (is *InsertionSort) Sort(s []int) ([]int, error) {
+	is.s = make([]int, len(s))
+	copy(is.s, s)
+
+	l := len(is.s)
+	for i := 1; i < l; i++ {
+		for j := i; j > 0; j-- {
+			if Less(is.s[j], is.s[j-1]) {
+				Exchange(is.s, j, j-1)
+			}
+		}
+	}
+
+	return is.s, nil
+}
--- insertion_sort_test.go
+++ insertion_sort_test.go
@@ -1,1 +1,20 @@
 package main
+
+import "testing"
+
+func TestInsertionSort(t *testing.T) {
+	s := []int{5, 2, 4, 1, 3}
+	expected := []int{1, 2, 3, 4, 5}
+
+	is := InsertionSort{}
+	r, err := is.Sort(s)
+	if err != nil {
+		t.Errorf("unexpected error: %e", err)
+	}
+
+	for i := 0; i < len(s); i++ {
+		if expected[i] != r[i] {
+			t.Errorf("expected: %d, got: %d", expected[i], r[i])
+		}
+	}
+}
